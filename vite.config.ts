a077c21bbf24bc813e321031b56ec485ea9565bc
ryanmoyo <iherewithmyheart@gmail.com>
2022-03-13 22:22:23 +0800

    🔧 lib mode config

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      utils: '/lib/utils'
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/createStore.js'),
+      name: '@useless/store',
+      fileName: format => `store.${format}.js`
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {}
+      }
+    }
+  }
+})
